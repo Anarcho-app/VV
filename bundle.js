a0ae1319aced2e2bbb62e533deacdecbac775fe9
Anarcho.App <107971748+Anarcho-app@users.noreply.github.com>
2024-05-25 14:16:34 -0400

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -46433,8 +46433,8 @@ function App() {
     autoComplete: true,
     autoCompleteDelay: 250
   });
-  const pmt = new PMTiles("https://build.protomaps.com/20240524.pmtiles");
-  const pmt_src = "https://build.protomaps.com/20240524.pmtiles";
+  const pmt = new PMTiles("https://corsproxy.io/?https://build.protomaps.com/20240524.pmtiles");
+  const pmt_src = "https://corsproxy.io/?https://build.protomaps.com/20240524.pmtiles";
   let firstRun = true;
   let mapIT = "";
   let mapIT2 = "";
